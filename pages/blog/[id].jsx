b6244247a508aa2a0aeb0511f1af464f8d7b6c99
Sanjeet N. Dasharath <sanjeetdasharath@gmail.com>
2021-08-12 12:40:30 +0530

    add boilerplate

---
+++ pages/blog/[id].jsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import { MDXRemote } from "next-mdx-remote";
+
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export default function BlogPost({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <MDXRemote {...postData.serializedMdx} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+
+  return { props: { postData } };
+}
